1d9787a5649ef982b7a2c52fb6cd076180f4d8b2
Romulo Rosa <romulorosadev@gmail.com>
2024-06-08 03:05:58 -0300

    query exclude

--- query.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE dbProduct;
-USE dbProduct;
-
-CREATE TABLE dbProduct (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(250) NOT NULL,
-    fabricante VARCHAR(250) NOT NULL,
-    preco DOUBLE
-);
